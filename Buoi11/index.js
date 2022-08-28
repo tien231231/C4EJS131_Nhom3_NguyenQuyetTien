1fbd9bb85c38ecb7afea5fe11ba07fd0cbf1208c
tien231231 <tien231ub@gmail.com>
2022-08-28 21:06:18 +0700

    new b11

--- Buoi11/index.js
+++ Buoi11/index.js
@@ -1,13 +1,15 @@
 let b = document.querySelector('button#b')
-let b2 = document.querySelector('button#b2')
+
 
 b.onclick = function () {
     main.style.background = "red";
 };
 
 let count =0;
-    b2.onclick = function(){
-        b2.innerHTML = count ;
-        count= count+1;
-
-    };
+let b2 = document.querySelector('button#b2');
+function upElements(){
+    
+    
+    b2.innerHTML = count;
+    count= count+1;
+    }    
